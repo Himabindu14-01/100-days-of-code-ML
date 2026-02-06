8c3a0a26be29069762af2d8dfd0116cf1d9608f1
HimaBindu Velisi <binduvelisi0814@gmail.com>
2026-02-06 06:04:02 +0530

    Add files via upload

---
+++ 100 days of python/day7.py
@@ -0,0 +1,9 @@
+#today is we are going to do string topics like methods
+s="hima"
+s1="bindu"
+print(s,s1)
+#string methods
+print(s.upper())  # Converts to uppercase
+print(s.capitalize())  # Capitalizes the first letter
+print(s1.lower())  # Converts to lowercase
+print(s1.title())  # Capitalizes the first letter of each word
